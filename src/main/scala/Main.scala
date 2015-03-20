eb092e54f9c8f52ce34080a29108ff442e1741e5
Victor <gafiatulin@gmail.com>
2015-03-20 12:24:12 +0300

    input from stdin or file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,16 @@
 
 object Main{
   def main(args: Array[String]) = {
-
+    val text = Try{
+      args match {
+        case Array(key, filename, _*) =>
+          if(key == "-f") io.Source.fromFile(filename).mkString
+          else throw new IllegalArgumentException("Not supported argument:  " ++ key)
+        case Array(key, _*) =>
+          throw new IllegalArgumentException("No value for argument: " ++ key)
+        case _ =>
+          io.Source.stdin.getLines.takeWhile(_.nonEmpty).mkString(" ") + " "
+      }
+    }
   }
 }
